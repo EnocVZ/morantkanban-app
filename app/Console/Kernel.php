f5db3c18e8b0b6f584e722430d31774c24691862
Enocvz <35532625+EnocVZ@users.noreply.github.com>
2026-02-11 23:09:15 -0600

    feat: change cron option

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,8 +29,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('app:archive-task-expired-subcolumn')
-        //->daily()
-        ->hourly()
+        ->daily()
+        //->hourly()
         ->timezone('America/Mexico_City');
     }
 
